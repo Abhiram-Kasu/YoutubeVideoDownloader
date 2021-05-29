f2541b711b0254bf3212358cfb2af7f671eb9b25
Abhiram Kasu <abhiram.kasu@gmail.com>
2021-05-28 21:06:42 -0600

    Used Youtube Exploder to get higher quality which caused me so much pain but its fine since it works but it works slow So I have to work on that

--- YoutubeVideoDownloader/MainWindow.xaml.cs
+++ YoutubeVideoDownloader/MainWindow.xaml.cs
@@ -15,7 +15,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
-using VideoLibrary;
+
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+using YoutubeExplode.Converter;
+
+
 
 namespace YoutubeVideoDownloader
 {
@@ -26,6 +31,7 @@ namespace YoutubeVideoDownloader
     {
         String path;
         int start;
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -65,32 +71,90 @@ namespace YoutubeVideoDownloader
             Thread.Sleep(500);
 
         }
+        public void document(String content) {
+
+            debugConsole.Text += String.Format("{0} " + content + "\r\n", DateTime.Now.ToString());
+            DoEvents();
+        }
 
         
-        public void Download() {
-            start = 0;
-
-            var youTube = YouTube.Default; // starting point for YouTube actions
-            updateToo(25);
+        public async void Download(bool? mp4) {
+            String link = inputURL.Text;
+            link.Replace("https://www.youtube.com/watch?v=", "");
             
-            var video = youTube.GetVideo(inputURL.Text); // gets a Video object with info about the video
-            updateToo(50);
-
-            debugConsole.Text += String.Format("{0} Got youtube video object\r\n", DateTime.Now.ToString());
+            
+            var youtube = new YoutubeClient();
+            var video = await youtube.Videos.GetAsync(link);
+            updateToo(20);
+            document("Got video and created youtube client");
+            
             path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            path += @"\" + video.FullName;
-            updateToo(75);
-            debugConsole.Text += String.Format("{0} Path of Downloaded Object is: \r\n" + path + "\r\n", DateTime.Now.ToString());
-            File.WriteAllBytes(path, video.GetBytes());
-            updateToo(100);
+            path += @"\" + video.Title;
+            updateToo(40);
+            document("Created path with video name as file");
+
+            
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(link);
+            var audiostream = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
+            updateToo(60);
+            document("Got audio Stream");
+            if ((bool)mp4)
+            {
+                path += ".mp3";
+                await youtube.Videos.Streams.DownloadAsync(audiostream, path);
+                String mp4path = path.Substring(0, path.Length - 4);
+                mp4path += ".mp4";
+                
+                
+                var videostream = streamManifest.GetVideoOnlyStreams().Where(s => s.Container == Container.Mp4).GetWithHighestVideoQuality();
+                document("Got video stream");
+                await youtube.Videos.Streams.DownloadAsync(videostream, mp4path);
+
+                document("Downloaded Video to : " + mp4path);
+                DoEvents();
+                
+                var combinedstreams = new IStreamInfo[] { audiostream, videostream };
+                ProgressBar.IsIndeterminate = true;
+                DoEvents();
+
+                await youtube.Videos.DownloadAsync(combinedstreams, new ConversionRequestBuilder(mp4path).Build());
+
+                /*await youtube.Videos.DownloadAsync(link, path);*/
+              
+
+                updateToo(80);
+                ProgressBar.IsIndeterminate = false;
+                DoEvents();
+                document(path);
+                /*await youtube.Videos.DownloadAsync(combinedstreams, new ConversionRequestBuilder("Test.mp4").Build());*/
+                document("Downloaded MP4 file to: " + path);
+                updateToo(100);
+            }
+            else {
+                path += ".mp3";
+                await youtube.Videos.Streams.DownloadAsync(audiostream, path);
+                updateToo(100);
+                document("Downlaoded audio file to:" + path);
+            }
+            
+            
+
+
+            
+            
+            
+           
+            
+            
 
         }
 
         private void downloadButton_Click(object sender, RoutedEventArgs e)
         {
-            Download();
+            Download(boolMP4.IsChecked);
         }
 
+      
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Clipboard.SetText(path);
